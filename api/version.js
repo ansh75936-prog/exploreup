0b24c7915726a59f0bfb5bc3afdf3202153bb73c
ansh75936-prog <goku75936@gmail.com>
2026-09-24 14:53:09 +0530

    Add deployment version endpoint for PWA updates

---
+++ api/version.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.status(200).json({
+    version: process.env.VERCEL_GIT_COMMIT_SHA || process.env.VERCEL_DEPLOYMENT_ID || 'unknown'
+  });
+}
